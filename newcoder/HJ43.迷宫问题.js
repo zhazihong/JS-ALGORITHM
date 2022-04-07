626e22b506cc8578c588368f2a0dd3ad37b53c2a
zhazihong <1982780677@qq.com>
2022-04-07 11:41:07 +0800

    HJ43. 迷宫问题

--- newcoder/HJ43.迷宫问题.js
+++ newcoder/HJ43.迷宫问题.js
@@ -1,32 +1,35 @@
 var maze = [
-    [0, 1, 0, 0, 0],
-    [0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 0],
-    [0, 0, 0, 1, 0]
+    [0, 1, 0, 0, 0,],
+    [0, 1, 1, 1, 0,],
+    [0, 0, 0, 0, 0,],
+    [0, 1, 1, 1, 0,],
+    [0, 0, 0, 1, 0,],
 ];
 var row = maze.length;
 var col = maze[0].length;
-var arr = [];
+var visited = new Array(row).fill('').map(() => new Array(col).fill(false));
 var res = [];
 
 // 图深度优先遍历
-const dfs = (r, c) => {
-    arr.push(`${r},${c}`);
-    if (r + 1 < row && maze[r + 1][c] === 0) {
-        dfs(r + 1, c);
+const dfs = (r, c, path) => {
+    visited[r][c] = true;
+    // 递归终点
+    if (r === row - 1 && c === col - 1) {
+        res = path;
+        return;
     }
-    if (c + 1 < col && maze[r][c + 1] === 0) {
-        dfs(r, c + 1);
+    if (r + 1 < row && maze[r + 1][c] === 0 && !visited[r + 1][c]) {
+        dfs(r + 1, c, [...path, `${r + 1},${c}`]);
+    }
+    if (c + 1 < col && maze[r][c + 1] === 0 && !visited[r][c + 1]) {
+        dfs(r, c + 1, [...path, `${r},${c + 1}`]);
+    }
+    if (r - 1 >= 0 && maze[r - 1][c] === 0 && !visited[r - 1][c]) {
+        dfs(r - 1, c, [...path, `${r - 1},${c}`]);
+    }
+    if (c - 1 >= 0 && maze[r][c - 1] === 0 && !visited[r][c - 1]) {
+        dfs(r, c - 1, [...path, `${r},${c - 1}`]);
     }
 }
-dfs(0, 0);
-res.push(arr[arr.length - 1]);
-for (let i = arr.length - 2; i >= 0; i--) {
-    const [r, c] = arr[i].split(',');
-    const [r1, c1] = res[0].split(',');
-    if ((Number(r) + Number(c)) == (Number(r1) + Number(c1) - 1)) {
-        res.unshift(arr[i]);
-    }
-}
+dfs(0, 0, ['0,0']);
 res.forEach((item) => console.log(`(${item})`))
