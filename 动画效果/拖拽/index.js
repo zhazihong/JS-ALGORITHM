9634cdae6283f3acf84c5f869dc4a0767a4bc0dd
zhazihong <1982780677@qq.com>
2023-05-25 18:29:11 +0800

    简单拖拽效果

--- 动画效果/拖拽/index.js
+++ 动画效果/拖拽/index.js
@@ -1,24 +1,17 @@
-// const el = document.getElementsByClassName('card')[0];
-// console.log(el);
-// el.addEventListener('drag', () => {
-//     console.log('111');
-//     const offSet = el.offsetTop;
-//     console.log(offSet);
-// })
 const el = document.querySelector('.card');
-console.log(el);
 el.onmousedown = (e) => {
-    console.log('down');
-    let x = e.pageX - el.offsetLeft;
-    let y = e.pageY - el.offsetTop;
+    // e.pageX 获取鼠标点击位置 left
+    // offsetLeft 获取元素 left
+    const x = e.pageX - el.offsetLeft;
+    const y = e.pageY - el.offsetTop;
+    console.log(x);
     window.onmousemove = (e) => {
-        const cx = e.pageX - x;
-        const cy = e.pageY - y;
-        el.style.left = cx + 'px';
-        el.style.top = cy + 'px';
+        const left = e.pageX - x;
+        const top = e.pageY - y;
+        el.style.left = left + 'px';
+        el.style.top = top + 'px';
     }
     window.onmouseup = () => {
-        console.log('up');
         window.onmousemove = null;
         window.onmouseup = null;
     }
